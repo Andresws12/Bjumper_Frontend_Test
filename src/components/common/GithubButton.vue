340bf2473ede538960bf4077aacc628a2b0fb9a3
Andres Hernandez <andres.hernandez@iskaypet.com>
2025-02-28 23:37:10 +0100

    Add github link

---
+++ src/components/common/GithubButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <a
+    class="github-button"
+    href="https://github.com/Andresws12/Bjumper_Frontend_Test"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <img src="@/assets/github.svg" alt="Github" width="30" height="30" />
+  </a>
+</template>
+
+<style lang="scss" scoped>
+.github-button {
+  position: absolute;
+  top: 20px;
+  right: 70px;
+  z-index: 20;
+  width: 40px;
+  height: 40px;
+}
+</style>
